b59261c2f0a4201f7d0cb3dd13666e0c2bde7bcf
sibims <sibima00@gmail.com>
2024-05-24 10:46:02 +0530

    created a function for making chunks using RecursiveCharacterTextSplitter

--- chains.py
+++ chains.py
@@ -1,14 +1,28 @@
 import fitz  # PyMuPDF
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
-
+#for extract data from the documents
 def get_documentdata(pdf_docs):
     text = ""
-
     for pdf in pdf_docs:
         document = fitz.open(pdf)
         for page_num in range(len(document)):
             page = document.load_page(page_num)
             text += page.get_text()
-
     document.close()
+    return text
+
+
+#for making chunks
+def get_document_chunks(document):
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size=100,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    document_chunks = text_splitter.create_documents(document)
+    return document_chunks
+        
